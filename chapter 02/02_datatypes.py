06b7315c4c513c100d45043966e5fb7c1f808e86
Himanshu-063 <himanhsu06092006@gmail.com>
2026-06-05 22:38:42 +0530

    add changes

---
+++ chapter 02/02_datatypes.py
@@ -0,0 +1,9 @@
+a=1  # a is an integer
+
+b=2.3 # b is a floating point number
+
+c= "ritik" # c is a string
+
+d= False # d is a boolean variable
+
+e= None # e is a none variable
---
+++ chapter 02/03_operators.py
@@ -0,0 +1,19 @@
+# Arithmetic operators 
+
+a=3
+b=4
+c=a+b
+print(c)
+
+# Assisnment operators
+
+a= 4-2
+print(a)
+b=6
+b+=3
+print(b)
+
+# Comparison operators
+
+d = 6>2
+print(d)
